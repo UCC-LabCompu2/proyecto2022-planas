558a87eb914b475be571e093657947d06282205e
SantiagoP <santiagoplanasm@gmail.com>
2022-06-14 11:25:02 -0300

    se agrego js

---
+++ Funciones.js
@@ -0,0 +1,23 @@
+/**Se envia el formulario con la informacion obtenida (nombre del jugador).
+ * @method pasarFormulario
+*/
+function pasarFormulario(){
+    let nombre, urlComp, pasar;
+    nombre = document.getElementById("name").value;
+    pasar=1;
+
+
+    if(nombre===""){
+        alert("Ingrese un nombre valido");
+        pasar=0;
+    }
+    if(nombre.length>20){
+        alert("Nombre demasiado grande");
+        pasar=0;
+    }
+    if(pasar===1) {
+        urlComp = "juego.html#" + nombre ;
+        window.open(urlComp, "_self");
+    }
+
+}
